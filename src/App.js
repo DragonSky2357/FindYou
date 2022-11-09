c84a610138cc0b32ac335cce024df7841a6dd0f0
ebo4015 <nsy5403@gmail.com>
2022-11-09 21:45:14 +0900

    Header수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useState} from 'react';
+import styled from "styled-components";
 import Navlogin from "./components/Navlogin";
+import {BrowserRouter, Route,Routes} from'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Navlogin></Navlogin>
+      <BrowserRouter>
+      <Navlogin 
+        logo="SW로고"
+        sign="로그인"
+        account="회원가입" 
+      ></Navlogin>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navlogin.js
+++ src/components/Navlogin.js
@@ -1,18 +1,87 @@
-import React from 'react';
-import "./Navlogin.css";
-function Navlogin() {
-  return (
-    <div className="header">
-      <p className="logo">SW 로고</p>
+import React, {useState} from 'react';
+import styled from "styled-components";
+import {Link} from 'react-router-dom';
 
-      <div className="navbar">
-        <ul className ="nav__ul">
-            <li><a href="#">로그인</a></li>
-            <li><a href="#">회원가입</a></li>
-        </ul>
-      </div>
-    </div>
+/*  
+  <Header> : Navlogin의 상위 div
+  <Logo> : 홈페이지 로고
+  <Navbar> : nav div
+
+    border은 css test용
+    1.글씨 크기? 
+    2. 반응형수치
+/   3. 로그인시 nav상태 바뀌기  로그인, 회원가입 -> 로그아웃 개인정보창
+*/
+function Navlogin(props) {
+   const [_logo, setLogo] = useState(props.logo); //Logo의 props
+   const [_sign, setSign] = useState(props.sign); //Navbar sign props, setEffect?
+   const[_account,setAccount] = useState(props.account); //Navbar account props, setEffect?
+  return (
+  <Header> 
+    <Logo>
+      <p>{_logo}</p>
+    </Logo>   
+
+    <Navbar>
+      <ul>
+        <li><Link to="#">{_sign}</Link></li>
+        <li><Link to="#">{_account}</Link></li>
+      </ul>
+    </Navbar>
+  </Header>
+    
   )
 }
 
-export default Navlogin
+export default Navlogin;
+
+const Header = styled.div` //Navlogin의 상위 div
+ display:grid;
+  
+ margin:0;
+ grid-template-columns: 200px 300px; //logo font크기와 연관있음
+ grid-template-rows: 100px;
+ column-gap: 100px; //width 작아져도 container item들의 간격 200px까지는 보임
+ 
+ justify-content:space-between;
+ align-items:center;
+ border:2px solid red;
+`
+const Logo = styled.div` //로고 
+ margin: 10px 0px 10px 15px;
+ padding:0px;
+ p {
+ border:1px solid black;
+ font-size:47px;
+ font-weight:bold;
+ color: #47b5ff;
+ text-align:center;
+ }
+`
+
+const Navbar = styled.div` //Nav전체 div
+    
+  
+  border:3px solid green;
+   margin-right:100px;
+   ul {
+    padding:0;
+    list-style-type: none;
+     display:grid;
+     grid-template-columns: 1fr 1fr;
+     border:1px solid yellow;
+     align-items:center
+   }
+   li{
+    padding:20px 10px;
+    border: 1px solid purple;
+    text-align:center;
+    
+      a{
+    text-decoration-line:none;  
+    color:#47B5FF;
+    font-weight:bold;
+    font-size:17px;
+      }
+   }    
+`
