236599cb28f9ad410eb221bd4e4c58a7b5f6d245
unknown <qkrdydals327@gmail.com>
2022-11-21 15:43:05 +0900

    update swagger user

--- backend/src/api/routes/user.js
+++ backend/src/api/routes/user.js
@@ -6,35 +6,44 @@ const { auth } = require("../../middleware/middleware");
 /**
  * @swagger
  * paths:
- *  /user/nickname:
- *   post:
+ *  /user:
+ *   get:
  *    tags:
  *    - user
- *    description: 닉네임 조회
- *    parameters:
- *    - in: body
- *      name: body
- *      required: true
- *      schema:
- *       properties:
- *        id:
- *         type: string
- *        pw:
- *         type: string
- *
+ *    description: 모든 유저 정보 가져오기
  *    responses:
  *     200:
- *      description: 닉네임 조회 성공
- *      schema:
- *       properties:
- *        message:
- *         type: string
+ *      description: 모든 유저 정보
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              sucess:
+ *                type: boolean
+ *                example: true
+ *              message:
+ *                type: string
+ *                example: "Get Users Sucess"
+ *              data:
+ *                type: object
+ *                example: [{"department":"컴퓨터소프트웨어공학과","studentId":"20174041","email":"test@naver.com","nickname":"dragonsky"},
+ *                          {"department":"의료IT공학과","studentId":"20175129","email":"test@gmail.com","nickname":"yongbum kim"}]
+ *      
  *     401:
- *      description: 닉네임 조회 실패
- *      schema:
- *       properties:
- *        message:
- *         type: string
+ *      description: 전체 유저 조회 실패
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              sucess:
+ *                type : boolean
+ *                example: false
+ *              message:
+ *                type: string
+ *                example: "Get Users Failed"
+ *      
  *
  */
 
@@ -50,6 +59,55 @@ router.get("/", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ *  /user/{studentId}:
+ *    get:
+ *      tags:
+ *      - user
+ *      description: 학번에 해당하는 유저 정보 가져오기
+ *      
+ *      parameters:
+ *      - in: path
+ *        name: studentId
+ *        required: true
+ *        description: 학번
+ *        schema:
+ *          type: string
+ *        example: 20174041
+ * 
+ *    responses:
+ *     200:
+ *      description: 해당 학생 정보
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              sucess:
+ *                type: boolean
+ *                example: true
+ *              message:
+ *                type: string
+ *                example: "Get User Sucess"
+ *              data:
+ *                type: object
+ *                example: [{"department":"컴퓨터소프트웨어공학과","studentId":"20174041","email":"test@naver.com","nickname":"dragonsky"}]
+ *      
+ *     401:
+ *      description: 유저 조회 실패
+ *      schema:
+ *       properties:
+ *        sucess:
+ *          type : boolean
+ *          example: false
+ *        message:
+ *          type: string
+ *          example: "Get User Failed"
+ *      
+ *
+ */
+
 // Find User By StudentId
 router.get("/:studentId", async (req, res) => {
   try {
@@ -64,8 +122,54 @@ router.get("/:studentId", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ *  /user/new:
+ *    post:
+ *      tags:
+ *      - user
+ *      description: 회원가입
+ *      
+ *      requestBody:
+ *        description: 회원가입 정보
+ *        required: ture
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                department:
+ *                  type: string
+ *                  description: "학과"
+ * 
+ *                studentId:
+ *                  type: integer
+ *                  description: "학번"
+ *            
+ *                id:
+ *                  type: string
+ *                  description: "아이디"
+ * 
+ *                password:
+ *                  type: string
+ *                  description: "비밀번호"
+ *                
+ *                email:
+ *                  type: string
+ *                  format: email
+ *                  description: "이메일"
+ * 
+ *                nickname:
+ *                  type: string
+ *                  description: "닉네임"
+ *                
+ * 
+ *        
+ *
+ */
+
 // Create User
-router.post("/", async (req, res) => {
+router.post("/new", async (req, res) => {
   try {
     const findUser = await User.findOneByStudentid(req.body.studentId);
 
@@ -78,6 +182,72 @@ router.post("/", async (req, res) => {
     res.status(500).send({ err });
   }
 });
+
+/**
+ * @swagger
+ *  /user/{studentId}:
+ *    patch:
+ *      tags:
+ *      - user
+ *      description: 회원정보 수정
+ *      
+ *      requestBody:
+ *        description: 회원정보 수정
+ *        required: ture
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                department:
+ *                  type: string
+ *                  description: "학과"
+ * 
+ *                password:
+ *                  type: string
+ *                  description: "비밀번호"
+ *                
+ *                email:
+ *                  type: string
+ *                  format: email
+ *                  description: "이메일"
+ * 
+ *                nickname:
+ *                  type: string
+ *                  description: "닉네임"
+ *                
+ *    responses:
+ *     200:
+ *      description: 해당 학생 정보
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              sucess:
+ *                type: boolean
+ *                example: true
+ *              message:
+ *                type: string
+ *                example: "Get User Sucess"
+ *              data:
+ *                type: object
+ *                example: [{"department":"컴퓨터소프트웨어공학과","studentId":"20174041","email":"test@naver.com","nickname":"dragonsky"}]
+ *      
+ *     401:
+ *      description: 유저 조회 실패
+ *      schema:
+ *       properties:
+ *        sucess:
+ *          type : boolean
+ *          example: false
+ *        message:
+ *          type: string
+ *          example: "Get User Failed"
+
+ *        
+ *
+ */
 
 // 학생 정보 수정
 router.patch("/:studentId", async (req, res) => {
@@ -99,6 +269,73 @@ router.patch("/:studentId", async (req, res) => {
   }
 });
 
+
+/**
+ * @swagger
+ *  /user/{studentId}:
+ *    delete:
+ *      tags:
+ *      - user
+ *      description: 회원 삭제
+ *      
+ *      requestBody:
+ *        description: 회원 삭제
+ *        required: ture
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                department:
+ *                  type: string
+ *                  description: "학과"
+ * 
+ *                password:
+ *                  type: string
+ *                  description: "비밀번호"
+ *                
+ *                email:
+ *                  type: string
+ *                  format: email
+ *                  description: "이메일"
+ * 
+ *                nickname:
+ *                  type: string
+ *                  description: "닉네임"
+ *                
+ *    responses:
+ *     200:
+ *      description: 해당 학생 정보
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              sucess:
+ *                type: boolean
+ *                example: true
+ *              message:
+ *                type: string
+ *                example: "Get User Sucess"
+ *              data:
+ *                type: object
+ *                example: [{"department":"컴퓨터소프트웨어공학과","studentId":"20174041","email":"test@naver.com","nickname":"dragonsky"}]
+ *      
+ *     401:
+ *      description: 유저 조회 실패
+ *      schema:
+ *       properties:
+ *        sucess:
+ *          type : boolean
+ *          example: false
+ *        message:
+ *          type: string
+ *          example: "Get User Failed"
+
+ *        
+ *
+ */
+
 // 학생 삭제
 router.delete("/:studentId", async (req, res) => {
   try {
@@ -114,6 +351,62 @@ router.delete("/:studentId", async (req, res) => {
   } catch (err) {}
 });
 
+/**
+ * @swagger
+ *  /user/login:
+ *    post:
+ *      tags:
+ *      - user
+ *      description: 로그인
+ *      
+ *      requestBody:
+ *        description: 로그인
+ *        required: ture
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                id:
+ *                  type: string
+ *                  description: "아이디"
+ * 
+ *                password:
+ *                  type: string
+ *                  description: "비밀번호"
+ *                
+ *    responses:
+ *     200:
+ *      description: 해당 학생 정보
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              sucess:
+ *                type: boolean
+ *                example: true
+ *              message:
+ *                type: string
+ *                example: "Get User Sucess"
+ *              data:
+ *                type: object
+ *                example: [{"department":"컴퓨터소프트웨어공학과","studentId":"20174041","email":"test@naver.com","nickname":"dragonsky"}]
+ *      
+ *     401:
+ *      description: 유저 조회 실패
+ *      schema:
+ *       properties:
+ *        sucess:
+ *          type : boolean
+ *          example: false
+ *        message:
+ *          type: string
+ *          example: "Get User Failed"
+
+ *        
+ *
+ */
 // Login User
 router.post("/login", async (req, res) => {
   const { id, password } = req.body;
