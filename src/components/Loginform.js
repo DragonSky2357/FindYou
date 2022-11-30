4f57a310731bb50c536eb458ccee569b5c73bf0a
Chaemin-Lim <antraxmin@naver.com>
2022-11-30 17:11:37 +0900

    Update Loginform.js - UI 업데이트

--- src/components/Loginform.js
+++ src/components/Loginform.js
@@ -2,27 +2,25 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-// 전체 화면 Grid 컴포넌트
 const Box = styled.div`
   display: grid;
   background: white;
   place-items: center;
   min-height: 100vh;
 `;
-// 로그인 폼과 헤더 h1을 포함하는 중앙 박스 컴포넌트
+
 const Login_Box = styled.div`
   width: 400px;
   background: #ffffff;
 `;
 
-// '로그인' 텍스트 h1 컴포넌트
 const Login_header = styled.h1`
   font-size: 24px;
   text-align: center;
   margin-bottom: 0px;
   margin-top: 30px;
 `;
-// 입력폼, 버튼을 포함하는 컴포넌트
+
 const Login_form = styled.form`
   position: relative;
   z-index: 1;
@@ -31,18 +29,19 @@ const Login_form = styled.form`
   text-align: center;
   padding: 20px 45px 45px 45px;
 `;
-// 아이디, 비밀번호를 입력하는 input 컴포넌트
+
 const Login_input = styled.input`
   font-size: 14px;
   padding: 15px;
-  background: #f2f2f2;
+  //background: #f2f2f2;
   width: 100%;
   margin-bottom: 10px;
   box-sizing: border-box;
   place-items: center;
   outline: none;
+  border: 1px solid #999;
 `;
-// 로그인 버튼 컴포넌트
+
 const Login_button = styled.button`
   background: #47b5ff;
   width: 100%;
@@ -50,8 +49,11 @@ const Login_button = styled.button`
   color: #ffffff;
   font-size: 14px;
   cursor: pointer;
+  border: none;
+  border-radius: 3px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, .6);
 `;
-// 회원가입 페이지로 연결될 링크 컴포넌트
+
 const Login_toJoin = styled.p`
   margin: 15px 0 0;
   color: #000000;
@@ -83,25 +85,6 @@ function Loginform(props) {
       .catch();
     e.preventDefault();
   };
-
-  /* 아이디 비밀번호가 제출될때 실행되는 이벤트 핸들러
-    const onSubmitEvent = (e) => {
-        e.preventDefault();
-
-        let body = {
-          Id: id,
-          Password: pw,
-        };
-    
-        // action의 반환값을 dispatch
-        dispatch(loginUser(body)).then((response) => {
-          if (response.payload.loginSuccess) {
-            props.history.push('/');
-          } else {
-            alert('Error');
-          }
-        });
-    };*/
 
   return (
     <Box>
